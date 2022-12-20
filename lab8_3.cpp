f2f68f47fda7a2d1c555ce795b62616f41b4cc25
Pimmada <nina21551@gmail.com>
2022-12-20 16:20:44 +0700

    lab8_3.cpp completed

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+	if(x=='A'){
+		return 'Z';
+	}
+	if(x=='0'){
+		return '0';
+	}
+	if(x==toupper(x)){
+		return x-1;
+	}else{
+		return '0';
+	}
 }
 
 int main(){
